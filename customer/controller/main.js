4e85c4b5bd558fd91413190c26bd1c3dad63eb36
ErauQsPhan <mrsplol102@gmail.com>
2023-08-22 23:35:02 +0700

    update store interface

---
+++ customer/controller/main.js
@@ -0,0 +1,8 @@
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
---
+++ customer/model/model.js
@@ -0,0 +1,21 @@
+export class Products {
+  constructor(
+    _name,
+    _price,
+    _screen,
+    _blackCamera,
+    _frontCamera,
+    _img,
+    _desc,
+    _type
+  ) {
+    this.name = _name;
+    this.price = _price;
+    this.screen = _screen;
+    this.blackCamera = _blackCamera;
+    this.frontCamera = _frontCamera;
+    this.img = _img;
+    this.desc = _desc;
+    this.type = _type;
+  }
+}
