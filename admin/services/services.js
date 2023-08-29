48e3798db0a48870df95e4ca4181435b09c34548
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-08-29 23:06:54 +0700

    Update logic validate error show/hide

--- admin/services/services.js
+++ admin/services/services.js
@@ -51,6 +51,7 @@ function deleteProduct() {
 function createProductModalShow() {
   createProductModal.show();
   document.body.style.overflow = "hidden";
+  resetPreview();
 }
 
 function createProductModalHide() {
@@ -61,6 +62,7 @@ function createProductModalHide() {
 function updateProductModalShow() {
   updateProductModal.show();
   // document.body.style.overflow = "hidden";
+  resetPreview();
 }
 
 function updateProductModalHide() {
@@ -416,7 +418,7 @@ function resetPreview() {
   document.getElementById("frontCamera").value = "";
   document.getElementById("photo").value = "";
   document.getElementById("description").value = "";
-  document.getElementById("type").value = "";
+  document.getElementById("type").value = NOT_SELECTED_YET;
 
   document.getElementById("nameUpdate").value = "";
   document.getElementById("priceUpdate").value = "";
@@ -425,7 +427,7 @@ function resetPreview() {
   document.getElementById("frontCameraUpdate").value = "";
   document.getElementById("photoUpdate").value = "";
   document.getElementById("descriptionUpdate").value = "";
-  document.getElementById("typeUpdate").value = "";
+  document.getElementById("typeUpdate").value = NOT_SELECTED_YET;
 
   document.getElementById("namePreview").innerHTML = "";
   document.getElementById("pricePreview").innerHTML = "";
@@ -435,6 +437,24 @@ function resetPreview() {
   document.getElementById("photoPreview").innerHTML = "";
   document.getElementById("descriptionPreview").innerHTML = "";
   document.getElementById("typePreview").innerHTML = "";
+
+  document.getElementById("name-error").innerHTML = "";
+  document.getElementById("price-error").innerHTML = "";
+  document.getElementById("screen-error").innerHTML = "";
+  document.getElementById("backCamera-error").innerHTML = "";
+  document.getElementById("frontCamera-error").innerHTML = "";
+  document.getElementById("photo-error").innerHTML = "";
+  document.getElementById("description-error").innerHTML = "";
+  document.getElementById("type-error").innerHTML = "";
+
+  document.getElementById("nameUpdate-error").innerHTML = "";
+  document.getElementById("priceUpdate-error").innerHTML = "";
+  document.getElementById("screenUpdate-error").innerHTML = "";
+  document.getElementById("backCameraUpdate-error").innerHTML = "";
+  document.getElementById("frontCameraUpdate-error").innerHTML = "";
+  document.getElementById("photoUpdate-error").innerHTML = "";
+  document.getElementById("descriptionUpdate-error").innerHTML = "";
+  document.getElementById("typeUpdate-error").innerHTML = "";
 }
 
 function searchProduct() {
